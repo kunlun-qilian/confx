74ffe49c815b65adc5fb7066e56ca407ddaa51dd
pippozq <zengqi0529@163.com>
2023-07-31 16:09:48 +0800

    init

---
+++ docker_file.go
@@ -0,0 +1,86 @@
+package confx
+
+import (
+	"bytes"
+	"fmt"
+	"path/filepath"
+)
+
+type DockerConfig struct {
+	BuildImage   string
+	RuntimeImage string
+	GoProxy      GoProxyConfig
+}
+
+type GoProxyConfig struct {
+	ProxyOn bool
+	Host    string
+}
+
+func (c *DockerConfig) setDefaults() {
+	if c.BuildImage == "" {
+		c.BuildImage = "dockerproxy.com/library/golang:1.20-buster"
+	}
+	if c.RuntimeImage == "" {
+		c.RuntimeImage = "gcr.dockerproxy.com/distroless/static-debian11"
+	}
+	if c.GoProxy.ProxyOn {
+		if c.GoProxy.Host == "" {
+			c.GoProxy.Host = "https://goproxy.cn,direct"
+		}
+	}
+}
+
+func (c *Configuration) dockerfile() []byte {
+	c.dockerConfig.setDefaults()
+	dockerfile := bytes.NewBuffer(nil)
+	// builder
+	_, _ = fmt.Fprintf(dockerfile, "FROM %s AS build-env\n", c.dockerConfig.BuildImage)
+
+	_, _ = fmt.Fprintln(dockerfile, `
+FROM build-env AS builder
+`)
+	// go proxy
+	if c.dockerConfig.GoProxy.ProxyOn {
+		_, _ = fmt.Fprintln(dockerfile, fmt.Sprintf(`
+ARG GOPROXY=%s`, c.dockerConfig.GoProxy.Host))
+	}
+
+	_, _ = fmt.Fprintln(dockerfile, `
+WORKDIR /go/src
+COPY ./ ./
+
+# build
+RUN make build WORKSPACE=`+c.WorkSpace())
+
+	// runtime
+	_, _ = fmt.Fprintln(dockerfile, fmt.Sprintf(
+		`
+# runtime
+FROM %s`, c.dockerConfig.RuntimeImage))
+
+	_, _ = fmt.Fprintln(dockerfile, `
+COPY --from=builder `+filepath.Join("/go/src/cmd", c.WorkSpace(), c.WorkSpace())+` `+filepath.Join(`/go/bin`, c.Command.Use)+`
+`)
+	for _, envVar := range c.defaultEnvVars.Values {
+		if envVar.Value != "" {
+			if envVar.IsCopy {
+				_, _ = fmt.Fprintf(dockerfile, "COPY --from=builder %s %s\n", filepath.Join("/go/src/cmd", c.WorkSpace(), envVar.Value), filepath.Join("/go/bin", envVar.Value))
+			}
+			if envVar.IsExpose {
+				_, _ = fmt.Fprintf(dockerfile, "EXPOSE %s\n", envVar.Value)
+			}
+		}
+	}
+
+	fmt.Fprintf(dockerfile, `
+ARG PROJECT_NAME
+ARG PROJECT_VERSION
+ENV PROJECT_NAME=${PROJECT_NAME} PROJECT_VERSION=${PROJECT_VERSION}
+
+WORKDIR /go/bin
+ENTRYPOINT ["`+filepath.Join(`/go/bin`, c.Command.Use)+`"]
+`)
+
+	return dockerfile.Bytes()
+}
